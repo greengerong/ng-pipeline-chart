1b1590e74ad87c937ebfd4b744c8d52009c3fdf6
polang <grzx2210@163.com>
2017-07-27 22:54:13 +0800

    tech

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('ng-pipeline-chart App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Pipeline stages');
   });
 });
